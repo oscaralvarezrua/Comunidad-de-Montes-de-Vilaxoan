9b7cee7eab2438f08e3db17e8764e21916161a5e
Oscar Alvarez <oscaralvarez4c@gmail.com>
2025-07-22 22:26:58 +0200

    deploy git fixed 3

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Comunidad-de-Montes-de-Vilaxoan/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
 });
